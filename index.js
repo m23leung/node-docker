e0321052c7e020d45d144ceb406a9c93e0b2458a
m23leung <crimsondarkn@192-168-1-107.tpgi.com.au>
2024-04-05 18:59:51 +1100

    update message

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(session({
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-    res.send("<h2>Hi There NARUTO!</h2>");
+    res.send("<h2>Hi There AUSSIE!</h2>");
     console.log("yeah it ran");
 });
 
